f58a68d706dd06998eb275d62aa87919ef622d51
VLADISLAW9 <80681837+VLADISLAW9@users.noreply.github.com>
2023-06-25 17:05:21 +0700

    bug fix in YandexAds

--- client-side/src/app/components/ui/Advertisement/YandexAd_grid.tsx
+++ client-side/src/app/components/ui/Advertisement/YandexAd_grid.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react'
 
 const YandexAd_grid: FC = () => {
-	return <div id='yandex_rtb_R-A-2461588-2'></div>
+	return <div id="yandex_rtb_R-A-2461588-3"></div>
 }
 
 export default YandexAd_grid
