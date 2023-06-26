307571293b4f0a7103853e12d4de1aa5c6c4180f
VLADISLAW9 <80681837+VLADISLAW9@users.noreply.github.com>
2023-06-26 21:54:26 +0700

    bug fix

--- client-side/src/app/components/ui/Advertisement/YandexAd_grid.tsx
+++ client-side/src/app/components/ui/Advertisement/YandexAd_grid.tsx
@@ -1,8 +1,12 @@
-import { useEffect } from 'react'
-
 const YandexAd_grid = () => {
-
-	return <div id='yandex_rtb_R-A-2461588-4'></div>
+	return (
+		<div
+			id='yandex_rtb_R-A-2461588-4'
+			style={{ maxHeight: '53vh', maxWidth: '20vw' }}
+		>
+			<div style={{ height: '53vh', width: '20vw' }}></div>
+		</div>
+	)
 }
 
 export default YandexAd_grid
