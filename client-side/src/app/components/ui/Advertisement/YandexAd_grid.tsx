64aa931bff72900e5d092fea8038570bd68ac42b
VLADISLAW9 <80681837+VLADISLAW9@users.noreply.github.com>
2023-06-26 14:51:02 +0700

    some updates

--- client-side/src/app/components/ui/Advertisement/YandexAd_grid.tsx
+++ client-side/src/app/components/ui/Advertisement/YandexAd_grid.tsx
@@ -1,6 +1,7 @@
-import { FC } from 'react'
+import { useEffect } from 'react'
 
-const YandexAd_grid: FC = () => {
+const YandexAd_grid = () => {
+
 	return <div id='yandex_rtb_R-A-2461588-4'></div>
 }
 
