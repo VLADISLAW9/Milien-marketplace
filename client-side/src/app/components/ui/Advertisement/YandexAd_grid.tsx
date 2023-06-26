737078288b7b9864d28f19d51bd87079427dcd79
VLADISLAW9 <80681837+VLADISLAW9@users.noreply.github.com>
2023-06-26 22:11:35 +0700

    bug fix

--- client-side/src/app/components/ui/Advertisement/YandexAd_grid.tsx
+++ client-side/src/app/components/ui/Advertisement/YandexAd_grid.tsx
@@ -1,11 +1,13 @@
 const YandexAd_grid = () => {
 	return (
-		<div
-			id='yandex_rtb_R-A-2461588-4'
-			style={{ maxHeight: '500', maxWidth: '300px' }}
-		>
-			<div style={{ height: '500px', width: '300px' }}></div>
-		</div>
+		<li className='flex justify-center items-center'>
+			<div
+				id='yandex_rtb_R-A-2461588-4'
+				style={{ maxHeight: '500px', maxWidth: '337px' }}
+			>
+				<div style={{ height: '500px', width: '337px' }}></div>
+			</div>
+		</li>
 	)
 }
 
